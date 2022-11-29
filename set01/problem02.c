08cfd282d589847cfe2cc20a6f55ad6ee1112ad5
Ramya Raga <114480299+ramyaragan@users.noreply.github.com>
2022-11-29 14:02:28 +0530

    Update problem02.c

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  printf("Enter a:");
+  scanf("%d",&a);
+  printf("Enter b:");
+  scanf("%d",&b);
+  int sum = a+b;
+  printf("Sum of a and b is: %d", sum);
+  return 0;
+  }
